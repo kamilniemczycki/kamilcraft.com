41ba3104e05e35e2bda5419c300ae59ec8ee7230
Kamil Niemczycki <kamilniemczycki@gmail.com>
2021-04-07 10:32:27 +0200

    Added router

---
+++ src/views/About.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="about">
+    <h1>O mnie</h1>
+    <p>Witajcie!</p>
+    <p>Nazywam się Kamil Niemczycki, jestem studentem uczelni Państwowej Wyższej Szkoły Zawodowej im. Witelona w Legnicy.</p>
+  </div>
+</template>

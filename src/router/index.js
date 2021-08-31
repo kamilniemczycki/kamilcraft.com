d4537b3b70375b022cd0a01fed63f26aeefa3b90
Kamil Niemczycki <kamilniemczycki@gmail.com>
2021-08-31 19:01:46 +0200

    Repair scrolling

--- src/router/index.js
+++ src/router/index.js
@@ -53,7 +53,19 @@ const routes = [
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL ?? '/',
-  routes
+  routes,
+  scrollBehavior (to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else if (to.hash) {
+      return {
+        selector: to.hash,
+        behavior: 'smooth'
+      }
+    } else {
+      return { x: 0, y: 0 }
+    }
+  }
 })
 
 const title = ' :: ' + mainTitle

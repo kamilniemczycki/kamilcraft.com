47de5f3508d7e55c5bf3b0f9b89e4a9b4b9913e6
Kamil Niemczycki <kamilniemczycki@gmail.com>
2021-10-11 12:06:37 +0200

    Update about

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,20 +1,57 @@
 <template>
   <section id="about">
     <div class="container">
+      <p>
+        <strong>Nazywam się Kamil Niemczycki. Jestem młodym i ambitnym inżynierem oprogramowania. Specjalizuję się w tworzeniu frontendów i backendów.
+          W projektach wykorzystuję techologie oparte o PHP i JavaScript, tworząc skomplikowane i skalowalne aplikacje internetowe.</strong>
+      </p>
       <h2>Studia</h2>
       <p>
-        Nazywam się Kamil Niemczycki, jestem absolwentem Państwowej Wyższej Szkoły Zawodowej im. Witelona w Legnicy.
-        Studiowałem na kierunku <em>Informatyka</em> o specjalności <em>Programowanie Aplikacji Mobilnych i
-        Internetowych.</em>
+        Jestem absolwentem Państwowej Wyższej Szkoły Zawodowej im. Witelona w Legnicy. Studiowałem na kierunku <em>Informatyka</em> o specjalności <em>Programowanie Aplikacji Mobilnych i Internetowych.</em>
       </p>
+      <p>
+        Podczas studiów zdobyłem niezbędną wiedzę z zakresu projektowania, wdrażania i rozwoju aplikacji internetowych, mobilnych i desktopowych.
+        Zdobyłem także umiejętności pracy w zespole, które owocują tworzeniem merytorycznej dyskusji na temat części lub całości projektu.
+      </p>
+      <h3>Kursy</h3>
+      <p>W ramach studiów uzyskałem zaświadczenie dotyczące ukończenia kursu:
+        <a href="#"><strong>Architektura aplikacji internetowych opartych o framework Laravel</strong></a>.</p>
+      <p>Umożliwiło mi to szersze spojrzenie na możliwości jakie daje nam Laravel i wzbogaconie poprzednio zdobytej
+        wiedzy o nowe doświadczenia.</p>
+      <h2>Moje zainteresowania</h2>
+      <p>W wolnym czasie przyjmuje małe zlecenia na strony lub serwisy internetowe. Uczę się nowych języków
+        programowania lub frameworków w znanych mi już technologiach. Hobbistycznie, tworzę także rozwiązania do
+        problemów przy prywatnych projektach.</p>
     </div>
   </section>
 </template>
 
 <style lang="scss">
 section#about {
-  padding: 20px 0;
+  padding: 45px 0;
   background-color: #fafafa !important;
+
+  h2 {
+    margin-top: 25px;
+
+    &:first-of-type {
+      margin-top: 10px;
+    }
+  }
+
+  h3 {
+    font-size: 1.3em;
+    margin-top: 8px;
+    margin-bottom: 10px;
+  }
+
+  a {
+    color: #8D8D8D;
+
+    &:hover {
+      color: #A2CF00;
+    }
+  }
 }
 </style>
 
@@ -27,7 +64,7 @@ export default {
   mounted () {
     const header = {
       title: this.$route.meta.title,
-      description: 'I\'m a software engineer specialised in frontend and backend development for complex scalable web apps. I write about software development on my blog.'
+      description: 'Jestem młodym i ambitnym inżynierem oprogramowania. Specjalizuję się w tworzeniu frontendów i backendów.'
     }
     this.$store.commit('setHeader', header)
   },

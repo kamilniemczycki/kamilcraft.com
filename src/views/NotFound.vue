3398ed6468e6cbc991cc64cceff8d2d65f7e8d27
Kamil Niemczycki <kamilniemczycki@gmail.com>
2021-10-15 10:51:18 +0200

    Update not found page

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,6 +1,30 @@
 <template>
-  <div id="not-found">
-    <h1>Error 404</h1>
-    <p>Not found</p>
+  <div class="not-found">
+    <div class="container">
+      <header>
+        <h1>Błąd 404</h1>
+      </header>
+      <p>Szukana strona wygasła lub nie istnieje.</p>
+      <p>Sprawdź adres i spróbuj ponownie!</p>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'NotFound'
+}
+</script>
+
+<style lang="scss" scoped>
+.not-found {
+  .container {
+    h1 {
+      margin-bottom: 25px;
+    }
+
+    padding-top: 45px;
+    padding-bottom: 45px;
+  }
+}
+</style>

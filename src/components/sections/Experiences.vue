aac5c6897e66b8344b962a80598a1bd6dd260871
Kamil Niemczycki <kamilniemczycki@gmail.com>
2021-10-15 11:18:19 +0200

    Update experiences for technology

--- src/components/sections/Experiences.vue
+++ src/components/sections/Experiences.vue
@@ -15,6 +15,7 @@
                 v-for="(icon, keyIcon) in skill.icons"
                 :key="keyIcon" :class="icon.class"
                 :icon="['fab', icon.name]"
+                :alt="icon.alt"
                 :title="icon.title" />
             </div>
             <h3>{{ skill.header }}</h3>
@@ -42,11 +43,13 @@ export default {
           {
             class: 'icon-php',
             name: 'php',
+            alt: 'Ikona przedstawiająca język PHP',
             title: 'Ikona PHP'
           },
           {
             class: 'icon-laravel',
             name: 'laravel',
+            alt: 'Ikona przedstawiająca framework Laravel',
             title: 'Ikona Laravel'
           }
         ],
@@ -62,6 +65,7 @@ export default {
           {
             class: 'icon-js',
             name: 'js-square',
+            alt: 'Ikona przedstawiająca język JavaScript',
             title: 'Ikona JavaScript'
           }
         ],
@@ -75,7 +79,8 @@ export default {
           {
             class: 'icon-vue',
             name: 'vuejs',
-            title: 'Ikona Vue'
+            alt: 'Ikona przedstawiająca framework Vue.js',
+            title: 'Ikona Vue.js'
           }
         ],
         header: 'Vue',
@@ -89,47 +94,52 @@ export default {
           {
             class: 'icon-html5',
             name: 'html5',
+            alt: 'Ikona przedstawiająca HTML5',
             title: 'Ikona HTML5'
           },
           {
             class: 'icon-css3',
             name: 'css3-alt',
+            alt: 'Ikona przedstawiająca CSS3',
             title: 'Ikona CSS3'
           }
         ],
         header: 'HTML & CSS',
         showMore: false,
-        description: `Framework ten wspomaga mnie w pisaniu aplikacji frontendowej. Przyspiesza tworzenie warstwy
-        wizualnej, obsługę interakcji z użytkownikiem i bindowanie zdefiniowanych pól, które są odpowiednio renderowane
-        przez przeglądarkę. To wszystko, umożliwia łatwe łączenie CSS i HTML z logiką i otrzymanymi danymi z API.`
+        description: `HTML i CSS to już nieodłączne technologie, które w łatwy sposób umożliwiają nadanie wizualnej
+        spójności dla treści, obrazów lub ułożenia elementów. Nie raz, odpowednie zaprojektowanie interfejsu użytkownika
+        przyspiesza integrację z naszą stroną/sklepem. Ma to przełożenie na zyski dla obu stron - firmy (finanse)
+        i klienta (czas).`
       },
       {
         icons: [
           {
             class: 'icon-node',
             name: 'node',
+            alt: 'Ikona przedstawiająca Node.js',
             title: 'Ikona Node.js'
           }
         ],
         header: 'Node.js',
         showMore: false,
-        description: `Framework ten wspomaga mnie w pisaniu aplikacji frontendowej. Przyspiesza tworzenie warstwy
-        wizualnej, obsługę interakcji z użytkownikiem i bindowanie zdefiniowanych pól, które są odpowiednio renderowane
-        przez przeglądarkę. To wszystko, umożliwia łatwe łączenie CSS i HTML z logiką i otrzymanymi danymi z API.`
+        description: `Node jest wieloplatformowym środowiskiem uruchomieniowym, umożliwiającym uruchomienie kodu
+        JavaScript po stronie backendowej. Jest to bardzo przyjemny język, który ułatwia projektowanie logiki biznesowej
+        aplikacji, dla mniej wymagających serwisów pod względem analizy i przetwarzania danych.`
       },
       {
         icons: [
           {
             class: 'icon-sass',
             name: 'sass',
+            alt: 'Ikona przedstawiająca Sass',
             title: 'Ikona Sass'
           }
         ],
         header: 'Sass',
         showMore: false,
-        description: `Framework ten wspomaga mnie w pisaniu aplikacji frontendowej. Przyspiesza tworzenie warstwy
-        wizualnej, obsługę interakcji z użytkownikiem i bindowanie zdefiniowanych pól, które są odpowiednio renderowane
-        przez przeglądarkę. To wszystko, umożliwia łatwe łączenie CSS i HTML z logiką i otrzymanymi danymi z API.`
+        description: `Sass zwiększa czytelność kodu dla programisty. Zawiera abstrakcje znane z języków obiektowych,
+        przy czym ogranicza powtarzalność i umożliwia optymalizację kodu. Kompilacja napisanego stylu w Sass jest
+        przekształcana do formy czytelnej dla silnika graficznego przeglądarki, co przyspiesza proces generowania strony.`
       }
     ]
   },
